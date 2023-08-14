155aa9af893258065dda02347f722bec8557ad5c
QuocThienn <129751048+QuocThienn@users.noreply.github.com>
2023-08-14 21:11:29 +0700

    Add files via upload

---
+++ ren_luyen_tu_duy/cu_phap.js
@@ -0,0 +1,39 @@
+// các biến
+var a= 10; //number
+var b= "ehe" //string
+var c = true; //boolean
+var d = false; //boolean
+var r = null; //null
+var e //Undefined
+var w =[1,2,3,4,] //array
+var q = Symbol(4); //symboll
+
+// câu điều kiện
+var age =18
+if(age>= 18) {
+    console.log("ra đường ngủ")
+}
+// if
+
+var tre= false
+if(tre===true){
+    console.log("còn non")
+}else {
+    console.log("lớn rồi")
+}
+
+var ko;
+if(ko=== undefined) {
+    console.log("seen không rep")
+}else {
+    console.log("ehe")
+}
+
+var t ="";
+if(t=== "thien") {
+    console.log("đúng tên")
+}else if(t=== "") {
+    console.log("tên rỗng")
+}else {
+    console.log("tên lạ")
+}
