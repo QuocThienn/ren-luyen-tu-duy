a21bf2d2b7d07e331a2023ad359bf311c7bbc340
QuocThienn <129751048+QuocThienn@users.noreply.github.com>
2023-08-20 19:37:06 +0700

    Add files via upload

--- ren_luyen_tu_duy/cu_phap.js
+++ ren_luyen_tu_duy/cu_phap.js
@@ -37,3 +37,64 @@ if(t=== "thien") {
 }else {
     console.log("tên lạ")
 }
+
+
+
+// bài 42
+function bai42(n) {
+var s=0
+for (var k=0; k < n; k++) {
+     s += k;
+}
+
+console.log("Giá trị k lớn nhất để S(k) = 1 + 2 + ... + k < n là:" + s);
+console.log("giá trị k lớn nhất" + (n - 1));
+}
+
+
+// bài 43
+
+var nu = 100;
+
+var sochuso= 0;
+var themang= nu;
+
+if (nu === 0) {
+    sochuso = 1;
+}
+
+while (themang !== 0) {
+    sochuso += 1;
+    themang = Math.floor(themang / 10);
+}
+
+console.log("So chu so cua", nu , "là" , + sochuso);
+
+// bài 44
+
+var x = 1234567;
+
+var s = 0;
+var mang= x;
+for (;mang !== 0; mang = Math.floor(mang / 10)) {
+    s = s + mang % 10;
+}
+// while(mang !== 0) {
+//     s += mang % 10;
+//     mang = Math.floor(mang / 10);
+// }
+
+console.log("Tong la", + s);
+
+// bài 45
+ 
+var na = 224;
+var p = 1;
+var the = na;
+
+while (the !== 0) {
+    p *= the % 10;
+    the = Math.floor(the / 10);
+  }
+
+  console.log("Tich la", p);
